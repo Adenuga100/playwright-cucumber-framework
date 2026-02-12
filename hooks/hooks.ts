57e7c1072842ba5cd514d4da2b06f8d3e5cc3698
Adenuga100 <adenugaadeyemiisaac@gmail.com>
2026-02-12 12:35:47 -0800

    headless false

--- hooks/hooks.ts
+++ hooks/hooks.ts
@@ -4,13 +4,29 @@ import { chromium, Browser, Page } from "@playwright/test";
 let browser: Browser;
 let page: Page;
 
+// Before(async function () {
+//     browser = await chromium.launch({ headless: false });
+//     const context = await browser.newContext();
+//     this.page = await context.newPage(); // 'this' shares the page with step definitions
+// });
 Before(async function () {
-    browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
-    this.page = await context.newPage(); // 'this' shares the page with step definitions
+    browser = await chromium.launch({ headless: false }); // GitHub needs headless: true
+    this.context = await browser.newContext();
+    this.page = await this.context.newPage(); 
 });
 
 After(async function () {
-    await this.page.close();
-    await browser.close();
+    // Adding the '?' ensures it only tries to close if the object exists
+    await this.page?.close();
+    await this.context?.close();
+    
+    // browser is usually a global variable in this file
+    if (browser) {
+        await browser.close();
+    }
 });
+
+// After(async function () {
+//     await this.page.close();
+//     await browser.close();
+// });
